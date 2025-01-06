7903aca13d8f23db6bee97804aa4fe4ac9ea46e9
team587 <hedgehogs@team587.org>
2025-01-06 15:21:50 -0500

    Initial commit

---
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -0,0 +1,179 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/DriveSubsystem.h"
+
+#include <frc/geometry/Rotation2d.h>
+#include <units/angle.h>
+#include <units/angular_velocity.h>
+#include <units/velocity.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "LimelightHelpers.h"
+#include <frc/XboxController.h>
+
+#include "Constants.h"
+
+using namespace DriveConstants;
+
+DriveSubsystem::DriveSubsystem()
+    : m_frontLeft{kFrontLeftDrivingCanId, kFrontLeftTurningCanId,
+                  kFrontLeftChassisAngularOffset},
+      m_rearLeft{kRearLeftDrivingCanId, kRearLeftTurningCanId,
+                 kRearLeftChassisAngularOffset},
+      m_frontRight{kFrontRightDrivingCanId, kFrontRightTurningCanId,
+                   kFrontRightChassisAngularOffset},
+      m_rearRight{kRearRightDrivingCanId, kRearRightTurningCanId,
+                  kRearRightChassisAngularOffset},
+      m_odometry{kDriveKinematics,
+                 frc::Rotation2d(units::radian_t{
+                     //m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}),
+                     m_NavX.GetRotation2d().Radians()}),
+                 {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+                  m_rearLeft.GetPosition(), m_rearRight.GetPosition()},
+                 frc::Pose2d{}} {}
+
+void DriveSubsystem::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+
+  //Telemetry
+  frc::SmartDashboard::PutNumber("Yaw", m_NavX.GetYaw());
+  frc::SmartDashboard::PutNumber("Pitch", m_NavX.GetPitch());
+  frc::SmartDashboard::PutNumber("Roll", m_NavX.GetRoll());
+
+  std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  targetOffsetAngle_Horizontal = table->GetNumber("tx", 0.0);
+  targetOffsetAngle_Vertical = table->GetNumber("ty", 0.0);
+  double targetArea = table->GetNumber("ta", 0.0);
+  double targetSkew = table->GetNumber("ts", 0.0);
+
+  frc::SmartDashboard::PutNumber("LL tx", targetOffsetAngle_Horizontal);
+  frc::SmartDashboard::PutNumber("LL ty", targetOffsetAngle_Vertical);
+  //frc::SmartDashboard::PutNumber("LL ta", targetArea);
+  //frc::SmartDashboard::PutNumber("LL ts", targetSkew);
+
+  distanceAprilTag = 20/tan(targetOffsetAngle_Vertical * std::numbers::pi / 180.0);
+  frc::SmartDashboard::PutNumber("Distance April Tag", distanceAprilTag);
+  distanceOff = (distanceAprilTag - 70.0) / 39.37;
+  frc::SmartDashboard::PutNumber("Distance Off", distanceOff);
+
+  m_odometry.Update(frc::Rotation2d(units::radian_t{
+                        //m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}),
+                        m_NavX.GetRotation2d().Radians()}),
+                    {m_frontLeft.GetPosition(), m_rearLeft.GetPosition(),
+                     m_frontRight.GetPosition(), m_rearRight.GetPosition()});
+}
+
+void DriveSubsystem::Drive(units::meters_per_second_t xSpeed,
+                           units::meters_per_second_t ySpeed,
+                           units::radians_per_second_t rot,
+                           bool fieldRelative) {
+  
+  units::meters_per_second_t xSpeedOverride;
+
+  frc::XboxController m_driverController{OIConstants::kDriverControllerPort};
+  double rightTriggerValue = (m_driverController.GetRightTriggerAxis() * -.8) + 1.0;
+  if(m_driverController.GetBButton()){
+    //std::cout << "Button Read\n";
+    std::cout << (targetOffsetAngle_Horizontal * std::numbers::pi / 180.0);
+    fieldRelative = false;
+   if(distanceOff > 0.1 || distanceOff < -0.1){
+    xSpeedOverride = (units::meters_per_second_t)(0.5 * distanceOff) * -1.0;
+   }else{
+    xSpeedOverride = (units::meters_per_second_t)0;
+   }
+    frc::SmartDashboard::PutNumber("xSpeed Override", (double)xSpeedOverride);
+      if(targetOffsetAngle_Horizontal > 5.0){
+        rot = (units::radians_per_second_t)(targetOffsetAngle_Horizontal * std::numbers::pi / 180.0);
+      }
+      else if(targetOffsetAngle_Horizontal < (-5.0)){
+        rot = (units::radians_per_second_t)(targetOffsetAngle_Horizontal * std::numbers::pi / 180.0);
+      }
+  }
+
+  // Convert the commanded speeds into the correct units for the drivetrain
+  units::meters_per_second_t xSpeedDelivered =
+      xSpeed.value() * DriveConstants::kMaxSpeed;
+  units::meters_per_second_t ySpeedDelivered =
+      ySpeed.value() * DriveConstants::kMaxSpeed;
+  units::radians_per_second_t rotDelivered =
+      rot.value() * DriveConstants::kMaxAngularSpeed;
+
+  frc::SmartDashboard::PutNumber("xSpeed", (double)xSpeed);
+  frc::SmartDashboard::PutNumber("ySpeed", (double)ySpeed);
+  frc::SmartDashboard::PutNumber("rot", (double)rot);
+
+  auto states = kDriveKinematics.ToSwerveModuleStates(
+      fieldRelative
+          ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+                xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                frc::Rotation2d(units::radian_t{
+                    //m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}))
+                    m_NavX.GetRotation2d().Radians()}))
+          : frc::ChassisSpeeds{xSpeedDelivered, ySpeedDelivered, rotDelivered});
+
+  kDriveKinematics.DesaturateWheelSpeeds(&states, DriveConstants::kMaxSpeed);
+
+  auto [fl, fr, bl, br] = states;
+
+  m_frontLeft.SetDesiredState(fl);
+  m_frontRight.SetDesiredState(fr);
+  m_rearLeft.SetDesiredState(bl);
+  m_rearRight.SetDesiredState(br);
+}
+
+void DriveSubsystem::SetX() {
+  m_frontLeft.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{45_deg}});
+  m_frontRight.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{-45_deg}});
+  m_rearLeft.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{-45_deg}});
+  m_rearRight.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{45_deg}});
+}
+
+void DriveSubsystem::SetModuleStates(
+    wpi::array<frc::SwerveModuleState, 4> desiredStates) {
+  kDriveKinematics.DesaturateWheelSpeeds(&desiredStates,
+                                         DriveConstants::kMaxSpeed);
+  m_frontLeft.SetDesiredState(desiredStates[0]);
+  m_frontRight.SetDesiredState(desiredStates[1]);
+  m_rearLeft.SetDesiredState(desiredStates[2]);
+  m_rearRight.SetDesiredState(desiredStates[3]);
+}
+
+void DriveSubsystem::ResetEncoders() {
+  m_frontLeft.ResetEncoders();
+  m_rearLeft.ResetEncoders();
+  m_frontRight.ResetEncoders();
+  m_rearRight.ResetEncoders();
+}
+
+units::degree_t DriveSubsystem::GetHeading() {
+  //return frc::Rotation2d(
+             //units::radian_t{m_gyro.GetAngle(frc::ADIS16470_IMU::IMUAxis::kZ)}).Degrees();
+  return frc::Rotation2d(units::radian_t{m_NavX.GetRotation2d().Radians()}).Degrees();
+}
+
+void DriveSubsystem::ZeroHeading() { m_NavX.Reset(); }
+
+double DriveSubsystem::GetTurnRate() {
+  //return -m_gyro.GetRate(frc::ADIS16470_IMU::IMUAxis::kZ).value();
+  //GetRate might not be the right method to call
+  return -m_NavX.GetRate();
+}
+
+frc::Pose2d DriveSubsystem::GetPose() { return m_odometry.GetPose(); }
+
+void DriveSubsystem::ResetOdometry(frc::Pose2d pose) {
+  m_odometry.ResetPosition(
+      GetHeading(),
+      {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+       m_rearLeft.GetPosition(), m_rearRight.GetPosition()},
+      pose);
+}
---
+++ src/main/include/Constants.h
@@ -0,0 +1,106 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <frc/trajectory/TrapezoidProfile.h>
+#include <rev/SparkMax.h>
+#include <units/acceleration.h>
+#include <units/angular_acceleration.h>
+#include <units/angular_velocity.h>
+#include <units/current.h>
+#include <units/length.h>
+#include <units/velocity.h>
+
+#include <numbers>
+
+#pragma once
+
+/**
+ * The Constants header provides a convenient place for teams to hold robot-wide
+ * numerical or bool constants.  This should not be used for any other purpose.
+ *
+ * It is generally a good idea to place constants into subsystem- or
+ * command-specific namespaces within this header, which can then be used where
+ * they are needed.
+ */
+
+namespace DriveConstants {
+// Driving Parameters - Note that these are not the maximum capable speeds of
+// the robot, rather the allowed maximum speeds
+constexpr units::meters_per_second_t kMaxSpeed = 4.8_mps;
+constexpr units::radians_per_second_t kMaxAngularSpeed{2 * std::numbers::pi};
+
+constexpr double kDirectionSlewRate = 1.2;   // radians per second
+constexpr double kMagnitudeSlewRate = 1.8;   // percent per second (1 = 100%)
+constexpr double kRotationalSlewRate = 2.0;  // percent per second (1 = 100%)
+
+// Chassis configuration
+constexpr units::meter_t kTrackWidth =
+    0.6731_m;  // Distance between centers of right and left wheels on robot
+constexpr units::meter_t kWheelBase =
+    0.6731_m;  // Distance between centers of front and back wheels on robot
+
+// Angular offsets of the modules relative to the chassis in radians
+constexpr double kFrontLeftChassisAngularOffset = 0;
+constexpr double kFrontRightChassisAngularOffset = 0;
+constexpr double kRearLeftChassisAngularOffset = 0;
+constexpr double kRearRightChassisAngularOffset = 0;
+
+// SPARK MAX CAN IDs
+constexpr int kFrontLeftDrivingCanId = 18;
+constexpr int kRearLeftDrivingCanId = 16;
+constexpr int kFrontRightDrivingCanId = 11;
+constexpr int kRearRightDrivingCanId = 13;
+
+constexpr int kFrontLeftTurningCanId = 17;
+constexpr int kRearLeftTurningCanId = 15;
+constexpr int kFrontRightTurningCanId = 12;
+constexpr int kRearRightTurningCanId = 14;
+}  // namespace DriveConstants
+
+namespace ModuleConstants {
+// The MAXSwerve module can be configured with one of three pinion gears: 12T,
+// 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+// more teeth will result in a robot that drives faster).
+constexpr double kDrivingMotorPinionTeeth1 = 14.0;
+constexpr double kDrivingMotorPinionTeeth2 = 28.0;
+constexpr double kDrivingMotorPinionTeeth3 = 15.0;
+constexpr double kDrivenMotorPinionTeeth1 = 50.0;
+constexpr double kDrivenMotorPinionTeeth2 = 16.0;
+constexpr double kDrivenMotorPinionTeeth3 = 45.0;
+
+// Calculations required for driving motor conversion factors and feed forward
+constexpr double kDrivingMotorFreeSpeedRps =
+    (5820.0 / 60);  // NEO free speed is 5820 RPM (As of 11/18/2024)
+                    // Vortex free speed is 6784 RPM
+constexpr units::meter_t kWheelDiameter = 0.1016_m;
+constexpr units::meter_t kWheelCircumference =
+    kWheelDiameter * std::numbers::pi;
+// 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+// teeth on the bevel pinion
+constexpr double kDrivingMotorReduction = (kDrivenMotorPinionTeeth1 / kDrivingMotorPinionTeeth1)
+         * (kDrivenMotorPinionTeeth2 / kDrivingMotorPinionTeeth2)
+         * (kDrivenMotorPinionTeeth3 / kDrivingMotorPinionTeeth3);
+constexpr double kDriveWheelFreeSpeedRps =
+    (kDrivingMotorFreeSpeedRps * kWheelCircumference.value()) /
+    kDrivingMotorReduction;
+}  // namespace ModuleConstants
+
+namespace AutoConstants {
+constexpr auto kMaxSpeed = 5_mps; // 5.9 for the vortex and 5 for the neo :p
+constexpr auto kMaxAcceleration = 3_mps_sq;
+constexpr auto kMaxAngularSpeed = 3.142_rad_per_s;
+constexpr auto kMaxAngularAcceleration = 3.142_rad_per_s_sq;
+
+constexpr double kPXController = 0.5;
+constexpr double kPYController = 0.5;
+constexpr double kPThetaController = 0.5;
+
+extern const frc::TrapezoidProfile<units::radians>::Constraints
+    kThetaControllerConstraints;
+}  // namespace AutoConstants
+
+namespace OIConstants {
+constexpr int kDriverControllerPort = 0;
+constexpr double kDriveDeadband = 0.05;
+}  // namespace OIConstants
